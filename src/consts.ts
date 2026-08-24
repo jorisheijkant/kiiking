cca11cc0dc0c2cc4beebfc780239c3617129ba39
jorisheijkant <jorisheijkant@persgroep.net>
2026-08-24 21:45:10 +0200

    Basic SEO fixes

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Kiiking.com';
+export const SITE_DESCRIPTION =
+	'Kiiking is a sport full of adrenaline. Amusing entertainment for the whole family. Efficient universal physical training. Attractive magnet for outdoor events. The aim of Kiiking, as a sport, is to get over-the-spindle with the longest shaft. To add a centimetre to the shaft is making the getting over-the-spindle for the athlete as difficult as adding a centimetre to the high bar for high jumpers.';
